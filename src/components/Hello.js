f61c17db17f58d70097a761d98f9237df9ff48ac
hgb926 <hgb926@naver.com>
2024-06-12 10:27:54 +0900

    initial commit

---
+++ src/components/Hello.js
@@ -0,0 +1,17 @@
+import React from "react"; // import문은 고정
+
+class Hello extends React.Component {
+// 위 hello를 컴포넌트 라고 부른다.
+
+  render() {
+
+    return (
+      <>
+        <h1>하하호호</h1>
+        <h2>냠냠뇽뇽</h2>
+      </>
+    );
+  }
+}
+
+export default Hello;
