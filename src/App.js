6a73d793a5e69824f6b8bb8586479f5823fde597
hgb926 <hgb926@naver.com>
2024-06-14 09:25:29 +0900

    0613 조건부 렌더링 전

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
+import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
-import ExpenseList from './components/expenses/ExpenseList'
-import NewExpense from './components/new-expense/NewExpense'
-
-
+import ExpenseList from './components/expenses/ExpenseList';
+import NewExpense from './components/new-expense/NewExpense';
 
 const App = () => {
 
-    // 서버에서 지출항목 JSON 배열을 응답받음 (server에서 fetch해서 가져온 데이터)
+    // 서버에서 지출항목 JSON 배열을 응답받음
     const expenses = [
         {
             title: '치킨먹음',
             price: 30000,
-            date: new Date(2024,6-1,3)
+            date: new Date(2024, 6 - 1, 3)
         },
         {
             title: '족발먹음',
             price: 40000,
-            date: new Date(2023,12-1,17)
+            date: new Date(2023, 12 - 1, 17)
         },
         {
             title: '헬스장등록',
             price: 300000,
-            date: new Date(2024,6-1,12)
+            date: new Date(2024, 6 - 1, 12)
         },
         {
             title: '파파존스피자',
             price: 40000,
-            date: new Date(2022,3-1,4)
+            date: new Date(2022, 3 - 1, 4)
         },
         {
             title: '파리채',
             price: 1500,
-            date: new Date(2023,5-1,19)
+            date: new Date(2023, 5 - 1, 6)
         },
-    ]
+    ];
+
+    // 배열을 상태변수로 관리
+    const [expenseList, setExpenseList] = useState(expenses);
+
 
     // ExpenseForm에게 내려보낼 함수
-    const onAddExpense = (userInput) => {
-        console.log('App.js가 내려보낸 함수 호출!')
-        expenses.push(userInput)
-        console.log(expenses)
-    }
 
+    // setter에게 변경을 감지시키기 위해서는
+    // 새로운 값을 넣어야 한다. (주소값 이전)
+    const onAddExpense = (userInput) => setExpenseList([...expenseList, userInput]);
 
     return (
         <>
-            <NewExpense onSave={onAddExpense}/>
-            <ExpenseList expenses={expenses} />
+            <NewExpense onSave={onAddExpense} />
+            <ExpenseList expenses={expenseList} />
         </>
     );
-}
+};
 
 export default App;
-
