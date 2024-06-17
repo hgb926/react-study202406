c6daead2efca48b821e7dbdcb7ff7f1fce50ca61
hgb926 <hgb926@naver.com>
2024-06-17 09:18:54 +0900

    0617 동적 인라인 스타일 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -4,10 +4,20 @@ import Button from '../UI/Button';
 
 const CourseInput = ({ onAdd }) => {
 
+    // 목표 인풋에 입력한 값
     const [enteredText, setEnteredText] = useState('');
+
+    // 입력값 검증을 통과했는지 여부를 상태관리
+    const [isValid, setIsValid] = useState(true);
 
     const formSubmitHandler = e => {
         e.preventDefault();
+
+        if (enteredText.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+
         const newGoalObject = {
           id: Math.random().toString(),
           text: enteredText
@@ -18,14 +28,31 @@ const CourseInput = ({ onAdd }) => {
         setEnteredText('');
     };
 
+    const goalChangeHandler = e => {
+
+        const inputValue = e.target.value;
+
+        // 입력값 검증
+        if (inputValue.trim().length > 0) { // 글자수가 0보다 클때
+            setIsValid(true);
+        }
+
+        setEnteredText(inputValue)
+    }
+
+
     return (
         <form onSubmit={formSubmitHandler}>
             <div className="form-control">
                 <label>나의 목표</label>
                 <input
                     type="text"
-                    onChange={e => setEnteredText(e.target.value)}
+                    onChange={goalChangeHandler}
                     value={enteredText}
+                    style={{
+                        backgroundColor: isValid ? 'transparent' : 'red',
+                        borderColor: isValid ? 'black' : 'red',
+                    }}
                 />
             </div>
             <Button type="submit" >목표 추가하기</Button>
