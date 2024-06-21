a687a402df9f2b2e323c4e1ba47661a2747fd0ab
hgb926 <hgb926@naver.com>
2024-06-21 12:00:01 +0900

    0621 장바구니에 보낼 객체데이터 만들기

--- src/components/Food/Layout/Header.js
+++ src/components/Food/Layout/Header.js
@@ -5,7 +5,7 @@ import HeaderCartButton from './HeaderCartButton'
 // 정적 이미지 로딩
 import foodImage from '../../../src/assets/img/meals.jpeg';
 
-const Header = () => {
+const Header = ({ onShowCart }) => {
     const { header, 'main-image': mainImage } = styles;
     // 대시- 가 있는 클래스명은 위처럼 받아줘야 한다.
     // 아니면 className={style['main-image']}
@@ -13,7 +13,7 @@ const Header = () => {
         <>
             <header className={header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onShow={onShowCart}/>
             </header>
             <div className={mainImage}>
                 <img src={foodImage} alt="zz"/>
