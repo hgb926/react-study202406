1176938ae0b55634db969df03d0198ffcb5ff221
hgb926 <hgb926@naver.com>
2024-06-24 09:41:21 +0900

    0624 총액 상태값 중앙관리

--- src/components/Food/Cart/Cart.js
+++ src/components/Food/Cart/Cart.js
@@ -1,26 +1,28 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import styles from './Cart.module.scss';
 import CartModal from './CartModal';
-import CartContext from '../../../store/cart-context'
-import CartItem from './CartItem'
+import CartContext from '../../../store/cart-context';
+import CartItem from './CartItem';
 
 // const DUMMY_CART = [
-//     {
-//         id: 'c1',
-//         name: '스시',
-//         amount: 2,
-//         price: 46000,
-//     },
-//     {
-//         id: 'c2',
-//         name: '띠드버거',
-//         amount: 1,
-//         price: 12000,
-//     },
+//   {
+//     id: 'c1',
+//     name: '스시',
+//     amount: 2,
+//     price: 46000,
+//   },
+//   {
+//     id: 'c2',
+//     name: '띠드버거',
+//     amount: 1,
+//     price: 12000,
+//   },
 // ];
 const Cart = ({ onClose }) => {
 
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, totalPrice } = useContext(CartContext);
+
+    console.log('cart: ', cartItems);
 
     const {
         'cart-items': cartItemStyle,
@@ -43,10 +45,15 @@ const Cart = ({ onClose }) => {
             </ul>
             <div className={total}>
                 <span>주문 총액</span>
-                <span>58,000원</span>
+                <span>{new Intl.NumberFormat('ko-KR').format(totalPrice)}원</span>
             </div>
             <div className={actions}>
-                <button className={btnAlt} onClick={onClose}>닫기</button>
+                <button
+                    className={btnAlt}
+                    onClick={onClose}
+                >
+                    닫기
+                </button>
                 <button className={button}>주문</button>
             </div>
         </CartModal>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,9 +1,12 @@
 import React, { useReducer, useState } from 'react';
 import CartContext from './cart-context';
 
+// 중앙관리 상태값 (state)
 const defaultState = {
-    items: [], // 장바구니 배열
+    items: [], // 장바구니 배열 상태값
+    totalPrice: 0, // 총액 상태값
 };
+
 
 // reducer: 여러가지 복잡한 상태관리를 단순화시키며 중앙집중화한다.
 // 리듀서 함수 정의
@@ -16,9 +19,15 @@ const cartReducer = (state, action) => {
 
     if (action.type === 'ADD') { // 장바구니 추가
         // 상태 업데이트 코드
+        const updateCartItems = [...state.items, action.value];
+
+        // 총액 상태 업데이트
+                            // 기존 값       + 새로운 값
+        const updatePrice = state.totalPrice + (action.value.price * action.value.amount);
 
         return {
-            items: [...state.items, action.value]
+            items: updateCartItems,
+            totalPrice: updatePrice,
         }; // 새로운 상태
     } else if (action.type === 'REMOVE') { // 장바구니 제거
         return null; // 새로운 상태
@@ -47,8 +56,10 @@ const CartProvider = ({ children }) => {
     };
 
     // Provider가 실제로 관리할 상태들의 구체적인 내용들
+    // consumer가 실질적으로 가지고 가는 데이터.
     const cartContext = {
         cartItems: cartState.items, // 상태값
+        totalPrice: cartState.totalPrice, // 총액 상태값
         addItem: addItemHandler, // 상태를 업데이트하는 함수
         removeItem: id => {}, // 상태를 업데이트하는 함수
     };
