131716ae107b48cd30a07614412219cf87bb5154
hgb926 <hgb926@naver.com>
2024-07-03 11:42:07 +0900

    0703 loader2

--- src/components/RouteExample/components/EventList.js
+++ src/components/RouteExample/components/EventList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './EventList.module.scss'
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const EventList = ({ eventList }) => {
     
     const { events, list, item, content } = styles;
+
+    // loader데이터는 loader를 선언한 페이지 밑에있는 하위컴포넌트 어디서든 사용가능
+    // const data = useLoaderData();
+    
     
     return (
         <div className={events}>
@@ -13,11 +17,11 @@ const EventList = ({ eventList }) => {
                 {
                     eventList.map(ev => (
                         <li key={ev.id} className={item}>
-                            <Link to={ev.id.toString()}>
-                                <img src={ev.image} alt={ev.title} />
+                            <Link to={ev.id}>
+                                <img src={ev.img_url} alt={ev.title} />
                                 <div className={content}>
                                     <h2>{ev.title}</h2>
-                                    <time>{ev.date}</time>
+                                    <time>{ev.startDate}</time>
                                 </div>
                             </Link>
                         </li>
--- src/components/RouteExample/pages/EventDetail.js
+++ src/components/RouteExample/pages/EventDetail.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams, useLoaderData } from 'react-router-dom'
 
 const EventDetail = () => {
 
-    const {eventId: id} = useParams();
+    const { eventId: id } = useParams();
+
+    // const data = useLoaderData();
+    // console.log('loader data: ', data) // undefined
+    
+    // loader를 사용한 컴포넌트의 형제나 부모는 데이터를 받아올 수 없다.
 
     return (
         <>
