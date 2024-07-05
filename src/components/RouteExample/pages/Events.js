5b6930674a167677e71096a7f1d04ddd927b5ce4
hgb926 <hgb926@naver.com>
2024-07-05 12:16:39 +0900

    0705 lodash: debounce, throttle 무한스크롤

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import EventList from '../components/EventList';
-import { useLoaderData, json } from 'react-router-dom';
 import EventSkeleton from '../components/EventSkeleton';
+
+// npm install loadsh
+import { debounce, throttle } from 'lodash';
 
 const Events = () => {
 
     // loader가 리턴한 데이터 받아오기
     // const eventList = useLoaderData();
-    // console.log(eventList)
+    // console.log(eventList);
+
 
     // 서버에서 가져온 이벤트 목록
     const [events, setEvents] = useState([]);
@@ -15,16 +18,36 @@ const Events = () => {
     // 로딩 상태 체크
     const [loading, setLoading] = useState(false);
 
+    // 현재 페이지 번호
+    const [currentPage, setCurrentPage] = useState(1);
+
+    // 더이상 가져올 데이터가 있는지 확인
+    const [isFinish, setIsFinish] = useState(false);
+
+
     // 서버로 목록 조회 요청보내기
-    const loadEvents = async () => {
+    const loadEvents = async() => {
 
-        
+        if (isFinish) {
+            console.log("loading finished")
+        }
+
+        console.log('start loading...');
         setLoading(true);
-        const response = await fetch('http://localhost:8282/events/page/1?sort=date');
-        const events = await response.json();
 
-        setEvents(events);
-        setLoading(false)
+        const response = await fetch(`http://localhost:8282/events/page/${currentPage}?sort=date`);
+        const { events: loadedEvents, totalCount } = await response.json();
+
+
+        const updatedEvents = [...events, ...loadedEvents ];
+        setEvents(updatedEvents);
+        setLoading(false);
+        setCurrentPage(prevPage => prevPage+1)
+        console.log('end loading!!');
+
+        // 로딩이 끝나면 더 이상 가져올게 있는지 여부를 체크한다.
+        setIsFinish(totalCount === updatedEvents.length);
+
     };
 
     // 초기 이벤트 1페이지 목록 가져오기
@@ -32,31 +55,51 @@ const Events = () => {
         loadEvents();
     }, []);
 
+    // 스크롤 핸들러
+    const scrollHandler = throttle(() => {
+        console.log("scrollHandler");
+        if (loading ||
+            window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight
+        ) {
+            return;
+        }
+        loadEvents();
+    }, 2000);
+
+    // 스크롤 이벤트 바인딩
+    useEffect(() => {
+        window.addEventListener('scroll', scrollHandler);
+
+        return () => {
+            window.removeEventListener('scroll', scrollHandler);
+            scrollHandler.cancel(); // 스로틀 취소
+        }
+    }, [currentPage, loading]);
+
     return (
         <>
-            {/*<h1>Events Page</h1>*/}
             <EventList eventList={events} />
-            {loading && <EventSkeleton/>}
+            {loading && <EventSkeleton />}
         </>
     );
 };
+
 
 export default Events;
 
 // loader를 app.js로부터 아웃소싱
 // export const loader = async () => {
-//
-//     const response = await fetch(`http://localhost:8282/events/page/1?sort=date`);
-//
-//     if (response.status !== 200) {
-//         const errorText = await response.text();
-//       throw new json(
-//           { message: errorText },
-//         {
-//             status: response.status,
-//         }
-//       ); // ErrorPage로 연결.
-//     }
-//
-//     return response;
-// }
+
+//   const response = await fetch('http://localhost:8282/events/page/1?sort=date');
+
+//   if (!response.ok) {
+//     const errorText = await response.text();
+//     throw json(
+//       { message: errorText },
+//       {
+//         status: response.status
+//       }
+//     );
+//   }
+//   return response; // ok일 경우 events[]
+// };
